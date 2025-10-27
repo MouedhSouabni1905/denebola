6e6408e924c52e50b45fb0f35da3abc521ff8324
Mouedh Souabni <mouedhsmsouabni@gmail.com>
2025-10-27 18:21:19 +0100

    kanban board

--- setup_incomplete.sh
+++ setup_incomplete.sh
@@ -17,7 +17,7 @@ ln -s $HOME/.config/home-manager/home.nix $HOME/.config/sysconfig/desktop/denebo
 # ------------------------------------------------------
 
 # Installing some basic stuff
-yes | dnf install hyprland neovim dolphin ripgrep zsh go cargo
+yes | dnf install hyprland neovim dolphin ripgrep zsh go cargo pip
 yes | dnf remove firefox
 curl https://github.com/omeiirr/quran-cli/blob/main/install.sh | bash
 mkdir $HOME/.quran/
@@ -55,4 +55,6 @@ dnf install quickshell
 
 sudo curl -sL $(curl -s https://api.github.com/repos/5hubham5ingh/WallRizz/releases/latest | grep -Po '"browser_download_url": "\K[^"]+' | grep WallRizz) | tar -xz && sudo mv WallRizz /usr/bin/
 
-# 
+# Installing kanban board program
+pip install rich
+pip install kanban-cli
